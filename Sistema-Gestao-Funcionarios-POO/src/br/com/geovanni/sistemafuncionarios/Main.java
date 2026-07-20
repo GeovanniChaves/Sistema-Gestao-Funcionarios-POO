fc3df85063146ee2637b9c2c5d6e7e62f7f9023f
GeovanniChaves <chavesgeovanni0@gmail.com>
2026-07-20 14:59:45 -0300

    feat: implementa classe Funcionario com encapsulamento e construtores
    - Adiciona atributos privados à classe Funcionario
    - Implementa getters e setters para todos os atributos
    - Cria construtor vazio e construtor completo
    - Adiciona validação básica para idade
    - Testa a criação de objetos na classe Main utilizando ambos os construtores

--- Sistema-Gestao-Funcionarios-POO/src/br/com/geovanni/sistemafuncionarios/Main.java
+++ Sistema-Gestao-Funcionarios-POO/src/br/com/geovanni/sistemafuncionarios/Main.java
@@ -1,10 +1,16 @@
 package br.com.geovanni.sistemafuncionarios;
+import br.com.geovanni.sistemafuncionarios.model.Funcionario;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		Funcionario funcionario1 = new Funcionario();
+		
+		
+		Funcionario funcionario2 = new Funcionario("Geovanni", 25, "123", "dev", 2500.00);
+		
 	}
 
 }
--- Sistema-Gestao-Funcionarios-POO/src/br/com/geovanni/sistemafuncionarios/model/Funcionario.java
+++ Sistema-Gestao-Funcionarios-POO/src/br/com/geovanni/sistemafuncionarios/model/Funcionario.java
@@ -2,4 +2,79 @@ package br.com.geovanni.sistemafuncionarios.model;
 
 public class Funcionario {
 
+	
+	private String nome;
+	private int idade;
+	private String cpf;
+	private String cargo;
+	private double salario;
+	
+	
+	public Funcionario() {
+			
+	}
+
+	
+	public Funcionario(String nome, int idade, String cpf, String cargo, double salario) {
+		this.nome = nome;
+		this.idade = idade;
+		this.cpf = cpf;
+		this.cargo = cargo;
+		this.salario = salario;
+	}
+	
+	
+	
+	public String getNome() {
+		return nome;		
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		if(idade >= 0) {			
+			this.idade = idade;
+		}
+		
+	}
+	
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+		
+	}
+	
+	
+	public String getCargo() {
+		return cargo;
+	}
+	
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+		
+	}
+	
+	
+	public double getSalario() {
+		return salario;
+	}
+	
+	public void setSalario(double salario) {
+		this.salario = salario;
+		
+	}
+	
+	
+	
 }
