3efe595955b56c0553a1927e3d4e0a9806b0e46f
GeovanniChaves <chavesgeovanni0@gmail.com>
2026-07-23 11:59:55 -0300

    feat: implementa classe Empresa com gerenciamento de funcionários
    - Adiciona atributos da classe Empresa
    - Implementa encapsulamento com getters e setters
    - Cria construtores vazio e completo
    - Inicializa a lista de funcionários no construtor padrão

--- Sistema-Gestao-Funcionarios-POO/src/br/com/geovanni/sistemafuncionarios/service/Empresa.java
+++ Sistema-Gestao-Funcionarios-POO/src/br/com/geovanni/sistemafuncionarios/service/Empresa.java
@@ -1,5 +1,40 @@
 package br.com.geovanni.sistemafuncionarios.service;
+import java.util.ArrayList;
+import br.com.geovanni.sistemafuncionarios.model.Funcionario;
 
 public class Empresa {
 
+	private String nomeEmpresa;	
+	private ArrayList<Funcionario> funcionarios;
+	
+	
+	public Empresa() {
+		funcionarios = new ArrayList<>();
+	}
+	
+	public Empresa(String nomeEmpresa, ArrayList<Funcionario> funcionarios) {
+		this.nomeEmpresa = nomeEmpresa;
+		this.funcionarios = funcionarios;
+	}
+	
+	
+	public String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+	
+	public void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
+	}
+	
+	
+	public ArrayList<Funcionario> getFuncionarios(){
+		return funcionarios;
+	}
+	
+	public void setFuncionarios(ArrayList<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+	
+	
+	
 }
